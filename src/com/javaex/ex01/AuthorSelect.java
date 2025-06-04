3b00c4cb007e1e78c419884a45f39d6a7698ef25
kangsb-010313 <kangsb668@naver.com>
2025-06-04 15:38:48 +0900

    jdbc 250604 15:38

--- src/com/javaex/ex01/AuthorSelect.java
+++ src/com/javaex/ex01/AuthorSelect.java
@@ -5,10 +5,16 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class AuthorSelect {
 
 	public static void main(String[] args) {
+		
+		//VO 벨류 오브젝트
+		List<AuthorVO> aList = new ArrayList<AuthorVO>();
+		
 		
 		// 0. import java.sql.*;
 		Connection conn = null;
@@ -26,11 +32,12 @@ public class AuthorSelect {
 		    // 3. SQL문 준비 / 바인딩 / 실행
 			//SQL문 준비 
 			String query = "";
+			//query += " select	author_id as id, ";
 			query += " select	author_id, ";
 			query += " 			author_name, " ;
-			query += " 			author_desc, "; 
+			query += " 			author_desc "; 
 			query += " from author ";
-			System.out.println(query);
+			// System.out.println(query); -- test
 		    
 			//바인딩
 			pstmt = conn.prepareStatement(query);
@@ -38,11 +45,32 @@ public class AuthorSelect {
 			//실행
 			rs = pstmt.executeQuery();
 			
-			
 		    // 4.결과처리
 			//rs에 표가 들어가 있다(사용이 불편함) --> 리스트에 담는다 
+			while(rs.next()) {
+				
+				
+				//ResultSet 의 데이터를 자바의 변수에 담는다
+				//int authorId = rs.getInt("id");
+				int authorId = rs.getInt("author_id");
+				String authorName = rs.getString("author_name");
+				String authorDesc = rs.getString("author_desc");
+				
+				/*
+				//숫자(순서)로 해도 가능함
+				int authorId = rs.getInt(1);
+				String authorName = rs.getString(2);
+				String authorDesc = rs.getString(3);
+				*/
+				
+				//자바의 데이터를 VO로 묶는다
+				AuthorVO authorVO = new AuthorVO(authorId, authorName, authorDesc);
+				
+				//VO를 리스트에 추가(add) 한다
+				aList.add(authorVO);
+				
+			}
 			
-
 		} catch (ClassNotFoundException e) {
 		    System.out.println("error: 드라이버 로딩 실패 - " + e);
 		} catch (SQLException e) {
@@ -64,11 +92,50 @@ public class AuthorSelect {
 		        System.out.println("error:" + e);
 		    }
 
-		}
+		}//finally
 
+		/*
+		for(int i=0; i<aList.size(); i++) {
+			
+			System.out.println(aList.get(i).toString());
+		}
+		*/
+		
+		//맨 앞 데이터 이름만 출력
+		//System.out.println(aList.get(0).getAuthorName());
+		
+		/*
+		------------------------------------
+		1. 이문열 (서울)
+		2. 이찬영 (미국)
+		------------------------------------
+		*/
 		
 		
 		
-	}
+		/*
+		System.out.println("------------------------------------");
+		
+		for(int i=0; i<aList.size(); i++) {
+			System.out.print(aList.get(i).getAuthorId() + ". ");
+			System.out.print(aList.get(i).getAuthorName());
+			System.out.println("(" + aList.get(i).getAuthorDesc() + ")");
+		}
+		System.out.println("------------------------------------");	
+		*/
+		System.out.println("------------------------------------");
+		
+		for(int i=0; i<aList.size(); i++) {
+			
+			int authorId = aList.get(i).getAuthorId();
+			String authorName = aList.get(i).getAuthorName();
+			String authorDesc = aList.get(i).getAuthorDesc();
+			
+			System.out.println(authorId + ". " + authorName + "(" + authorDesc + ")");
+		}
+		System.out.println("------------------------------------");	
+		
+		
+	}//main
 
 }
---
+++ src/com/javaex/ex02/BookAuthorVO.java
@@ -0,0 +1,12 @@
+package com.javaex.ex02;
+
+public class BookAuthorVO {
+
+	private int bookId;
+	private String title;
+	private String pubs;
+	private String pubDate;
+	private int authorId;
+	private String authorDesc;
+	
+}
